a9d8ffc8cd1ba3e6f09af43f16ac26ea9503ae68
Natasha Zebrev <zebre001@morris.umn.edu>
2021-10-28 11:30:42 -0500

    Implement Client
    Made the client able to handle multiple threads.
    
    Co-authored-by: helloworld12321 <walbr037@morris.umn.edu>

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -7,17 +7,68 @@ import java.net.Socket;
 
 public class EchoClient {
 	public static final int PORT_NUMBER = 6013;
+	public static final int BUFFER_SIZE = 1024;
 
-	public static void main(String[] args) throws IOException {
-		EchoClient client = new EchoClient();
-		client.start();
+	static InputStream socketIn;
+	static OutputStream socketOut;
+	static Socket socket;
+	public static void main(String[] args) throws IOException, InterruptedException {
+		String serverName;
+
+   	 // Use localhost unless we're given a server name on the command line.
+   	 if (args.length == 0) {
+     		 serverName = "127.0.0.1";
+   	 } else {
+     		 serverName = args[0];
+    	}
+
+    	try {
+      		socket = new Socket(serverName, PORT_NUMBER);
+     		socketIn = socket.getInputStream();
+      		socketOut = socket.getOutputStream();
+     		byte[] buffer = new byte[BUFFER_SIZE];
+     		int bytesRead;
+
+		Thread firstThread = new Thread(EchoClient::readFromUser);
+		firstThread.start();
+
+		Thread secondThread = new Thread(EchoClient::readFromServer);
+		secondThread.start();
+
+     	 	firstThread.join();
+		secondThread.join();
+
+      		socket.close();
+    	} catch (IOException ioe) {
+      		System.err.println("Unexpected exception when talking to server:");
+      		ioe.printStackTrace();
+    		}
 	}
 
-	private void start() throws IOException {
-		Socket socket = new Socket("localhost", PORT_NUMBER);
-		InputStream socketInputStream = socket.getInputStream();
-		OutputStream socketOutputStream = socket.getOutputStream();
 
-		// Put your code here.
+	public static void readFromUser(){
+		int byteRead;
+		try{
+			while((byteRead = System.in.read()) != -1){
+			socketOut.write(byteRead);
+			socketOut.flush();
+			}
+			socket.shutdownOutput();
+		} catch(IOException ioe){
+		System.err.println("Unexpected exception when talking to server:");
+		ioe.printStackTrace();
+		}
+	}
+	public static void readFromServer(){
+		int byteRead;
+		try{
+			while((byteRead = socketIn.read()) != -1){
+			System.out.write(byteRead);
+			System.out.flush();
+			} 	
+		} catch(IOException ioe){
+		System.err.println("Unexpected exception when reading from server:");
+		ioe.printStackTrace();
+		}
 	}
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -7,18 +7,51 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
+  public static final int PORT_NUMBER = 6013;
+  public static final int BUFFER_SIZE = 1024;
+
+  public static void main(String[] args) {
+    try {
+      ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
+
+      while (true) {
+        Socket socketToClient = serverSocket.accept();
+        InputStream in = socketToClient.getInputStream();
+        OutputStream out = socketToClient.getOutputStream();
+        byte[] buffer = new byte[BUFFER_SIZE];
+
+        // Read from the socket and echo any data back.
+        // When the client closes their "output" half of the socket, end the
+        // loop.
+        int bytesRead;
+        while ((bytesRead = in.read(buffer)) != -1) {
+          System.out.println("Yay I got data!!");
+		out.write(buffer, 0, bytesRead);
+          	out.flush();
+        }
+
+        // Then, close our "output" half of the socket.
+        socketToClient.shutdownOutput();
+      }
+    } catch (IOException ioe) {
+      System.err.println("Unexpected exception:");
+      ioe.printStackTrace();
+    }
+  }
+}
+//public class EchoServer {
 	
 	// REPLACE WITH PORT PROVIDED BY THE INSTRUCTOR
-	public static final int PORT_NUMBER = 0; 
-	public static void main(String[] args) throws IOException, InterruptedException {
-		EchoServer server = new EchoServer();
-		server.start();
-	}
+//	public static final int PORT_NUMBER = 0; 
+//	public static void main(String[] args) throws IOException, InterruptedException {
+//		EchoServer server = new EchoServer();
+//		server.start();
+//	}
 
-	private void start() throws IOException, InterruptedException {
-		ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
-		while (true) {
-			Socket socket = serverSocket.accept();
+//	private void start() throws IOException, InterruptedException {
+//		ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
+//		while (true) {
+//			Socket socket = serverSocket.accept();
 
 			// Put your code here.
 			// This should do very little, essentially:
@@ -26,6 +59,6 @@ public class EchoServer {
 			//   * Construct a Thread with your runnable
 			//      * Or use a thread pool
 			//   * Start that thread
-		}
-	}
-}
+//		}
+//	}
+//}
